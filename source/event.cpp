2ba023cab60618f94857f90002bc6dc86b71b6ea
hollymolly3309 <eric03190319@gmail.com>
2023-12-19 16:46:27 +0800

    符文完成

--- source/event.cpp
+++ source/event.cpp
@@ -39,30 +39,31 @@ void Event::loadPromptFrame(const std::string& color)
 {
     mtx.lock();
     ani::setPos(EVENT::POS::PROMPT_FRAME);
-    std::cout << color << unicode::CORNER_LINE_LU;
+    std::cout << color;
+    WRITE_UNI(unicode::CORNER_LINE_LU);
     for ( int i = 0; i < Global::Screen::winWith - 3; i++ )
     {
-        std::cout << unicode::LINE_HORIZONTAL;
+        WRITE_UNI(unicode::LINE_HORIZONTAL);
     }
-    std::cout << unicode::CORNER_LINE_RU;
+    WRITE_UNI(unicode::CORNER_LINE_RU);
     
     ani::setPos(EVENT::POS::PROMPT_FRAME);
     ani::moveCurse(CurserMove::MOVEDOWN,1);
-    std::cout << unicode::LINE_VERTICAL;
+    WRITE_UNI(unicode::LINE_VERTICAL);
     for ( int i = 0; i < Global::Screen::winWith - 3; i++ )
     {
         std::cout << ' ';
     }
-    std::cout << unicode::LINE_VERTICAL;
+    WRITE_UNI(unicode::LINE_VERTICAL);
 
     ani::setPos(EVENT::POS::PROMPT_FRAME);
     ani::moveCurse(CurserMove::MOVEDOWN,2);
-    std::cout << unicode::CORNER_LINE_LD;
+    WRITE_UNI(unicode::CORNER_LINE_LD);
     for ( int i = 0; i < Global::Screen::winWith - 3; i++ )
     {
-        std::cout << unicode::LINE_HORIZONTAL;
+        WRITE_UNI(unicode::LINE_HORIZONTAL);
     }
-    std::cout << unicode::CORNER_LINE_RD;
+    WRITE_UNI(unicode::CORNER_LINE_RD);
 
     std::cout << RESET;
     mtx.unlock();
--- source/graph.cpp
+++ source/graph.cpp
@@ -150,20 +150,26 @@ void graph::init()
     ///////////////////////BUFF RUNE圖形（星形）////////////////////
     pixel.change(unicode::FULL_PIXEL,ansi_color::background::RUNEBAG_BACKGROUND,ansi_color::font::RUNE_BUFF_STONE);
     BATTLE::ICON::RUNE_BUFF_ICON.graph[2][0] = pixel;
-    BATTLE::ICON::RUNE_BUFF_ICON.graph[0][1] = pixel;
     BATTLE::ICON::RUNE_BUFF_ICON.graph[1][1] = pixel;
     BATTLE::ICON::RUNE_BUFF_ICON.graph[3][1] = pixel;
     BATTLE::ICON::RUNE_BUFF_ICON.graph[4][1] = pixel;
     BATTLE::ICON::RUNE_BUFF_ICON.graph[2][2] = pixel;
 
-
     pixel.change(unicode::DOWN_PIXEL);
     BATTLE::ICON::RUNE_BUFF_ICON.graph[1][0] = pixel;
     BATTLE::ICON::RUNE_BUFF_ICON.graph[3][0] = pixel;
     pixel.change(unicode::UP_PIXEL);
-    BATTLE::ICON::RUNE_BUFF_ICON.graph[1][2] = pixel;
     BATTLE::ICON::RUNE_BUFF_ICON.graph[3][2] = pixel;
+
+    pixel.change(unicode::FULL_PIXEL,ansi_color::background::RUNEBAG_BACKGROUND,ansi_color::font::RUNE_BUFF_STONE_DARK);
+    BATTLE::ICON::RUNE_BUFF_ICON.graph[0][1] = pixel;
+    BATTLE::ICON::RUNE_BUFF_ICON.graph[2][2] = pixel;
+    pixel.change(unicode::UP_PIXEL);
+    BATTLE::ICON::RUNE_BUFF_ICON.graph[1][2] = pixel;
     
+    pixel.change(unicode::CORNER_PIXEL_NOLD,ansi_color::background::RUNE_BUFF_STONE,ansi_color::font::RUNE_BUFF_STONE_DARK);
+    BATTLE::ICON::RUNE_BUFF_ICON.graph[1][1] = pixel;
+
     pixel.change(unicode::RUNE_BUFF_SYMBOL,ansi_color::background::RUNE_BUFF_STONE,ansi_color::font::RUNE_BUFF_SYMBOL);
     BATTLE::ICON::RUNE_BUFF_ICON.graph[2][1] = pixel;
     ///////////////////////DEBUFF RUNE圖形（無人機形）////////////////////
